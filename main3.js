1722bd422172ccbe6f5fa4fb0b907dc2bac23cc5
AlvarezMariaBelen <beluchis1991@gmail.com>
2022-10-06 14:31:35 -0300

    main3.js

---
+++ main3.js
@@ -0,0 +1,19 @@
+let container = document.getElementById('container')
+for(let i=0; i< data.events.length; i++){
+    if(data.currentDate>data.events[i].date){
+    let div = document.createElement('div')
+    div.className = 'cards'
+    div.innerHTML += 
+       `
+        <div class="cards">
+        <img src="${data.events[i].image}" />
+            <h2>${data.events[i].name}</h2>
+            <p>${data.events[i].description}</p>
+            <p>${data.events[i].date}</p>
+            <p>${data.events[i].price}</p>
+            <a href="details.html"><button>More details</button></a>
+        </div>
+        `
+        container.appendChild(div)
+    }
+}
